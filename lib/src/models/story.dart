a28a61e82726761d790e2e1d4dae04ade35b57ea
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-25 19:01:44 -0700

    some progress on Story Brain

---
+++ lib/src/models/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  Story({
+    required this.title,
+    required this.choice1,
+    required this.choice2,
+  });
+
+  final String title;
+  final String choice1;
+  final String choice2;
+}
